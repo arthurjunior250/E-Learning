5494caec9865173ed82ad124414ad37bb291dab9
arthurjunior250 <arthurjunior88741@gmail.com>
2024-07-17 10:59:19 +0200

    ft-initial-commit

---
+++ frontend/src/Pages/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Home/Landing/Landing.css'
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div className="leftPart">
+          <p>Copyright  © 2024</p>
+        </div>
+      </div>
+  )
+}
+
+export default Footer
